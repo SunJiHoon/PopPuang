d56f92f41162739ab8e1160f2bc0690a9e37b9cb
SunJiHoon <cangogi1@naver.com>
2024-05-11 19:25:50 +0900

    프로젝트 생성 및 커밋

---
+++ PopPuang/src/main/java/putiez/PopPuang/PopPuangApplication.java
@@ -0,0 +1,13 @@
+package putiez.PopPuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopPuangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopPuangApplication.class, args);
+	}
+
+}
---
+++ PopPuang/src/test/java/putiez/PopPuang/PopPuangApplicationTests.java
@@ -0,0 +1,13 @@
+package putiez.PopPuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopPuangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
